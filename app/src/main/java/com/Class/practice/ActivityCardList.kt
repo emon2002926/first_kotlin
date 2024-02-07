5a565f779b82b2b8c3207e749d3e190918cd666e
emon <emon79605@gmail.com>
2024-02-08 02:18:19 +0600

    Initial commit Class practice 14

---
+++ app/src/main/java/com/Class/practice/ActivityCardList.kt
@@ -0,0 +1,11 @@
+package com.Class.practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityCardList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_list)
+    }
+}
